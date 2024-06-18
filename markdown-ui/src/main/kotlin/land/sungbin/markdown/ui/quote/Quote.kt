6621a7d698319a7284c3b52ca084fac22622d635
Ji Sungbin <ji@sungb.in>
2024-06-18 22:24:03 +0900

    Added Quote markdown text component

--- markdown-ui/src/main/kotlin/land/sungbin/markdown/ui/quote/Quote.kt
+++ markdown-ui/src/main/kotlin/land/sungbin/markdown/ui/quote/Quote.kt
@@ -1,3 +1,30 @@
 package land.sungbin.markdown.ui.quote
 
-// TODO
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.NonRestartableComposable
+import land.sungbin.markdown.runtime.MarkdownComposable
+import land.sungbin.markdown.ui.bufferOf
+import land.sungbin.markdown.ui.modifier.Modifier
+import land.sungbin.markdown.ui.text.AbstractText
+import land.sungbin.markdown.ui.text.Text
+import okio.BufferedSource
+
+@PublishedApi
+internal class QuoteText(value: BufferedSource) : AbstractText() {
+  init {
+    sink {
+      writeByte('>'.code)
+      writeByte(' '.code)
+      writeAll(value)
+    }
+  }
+}
+
+@Suppress("NOTHING_TO_INLINE")
+@[Composable NonRestartableComposable MarkdownComposable]
+public inline fun Quote(
+  text: CharSequence,
+  modifier: Modifier = Modifier,
+) {
+  Text(modifier = modifier, value = QuoteText(bufferOf(text)))
+}
